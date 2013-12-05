931dbf4b64ba184d3270028125919d9d60539309
TJH <tch399@gmail.com>
2013-12-05 13:40:50 -0500

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 LearnRails::Application.routes.draw do
+
+LearnRails::Application.routes.draw do
+  root to: 'visitors#new'
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
